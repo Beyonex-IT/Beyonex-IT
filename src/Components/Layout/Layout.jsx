7f27284dc467703d1c55485f27319dda47126f64
mahmoud <mahmoudamged00@gmail.com>
2026-04-09 15:13:33 +0300

    remove founding day

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -3,14 +3,14 @@ import { Outlet } from 'react-router-dom'
 import Navbar from '../Navbar/Navbar'
 import Footer from '../Footer/Footer'
 import FloatingButtons from '../FloatingButtons/FloatingButtons'
-import FoundingDayOverlay from '../FoundingDayOverlay/FoundingDayOverlay'
+
 import SEOManager from '../SEOManager/SEOManager'
 
 export default function Layout() {
   return (
     <>
       <SEOManager />
-      <FoundingDayOverlay />
+
       <Navbar />
       <main>
         <Outlet />
